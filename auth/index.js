96f8e7ab67a6bec4b8fc5f66c8185f1a8c6ccb51
Brunno Vodola <bvodola@gmail.com>
2020-01-28 16:39:24 -0300

    Minor change on auth

--- auth/index.js
+++ auth/index.js
@@ -1,41 +1,40 @@
-const { Users } = require('../models');
-const tokens = require('./tokens');
+const { Users } = require("../models");
+const tokens = require("./tokens");
 
-const register = (user) => {
+const register = user => {
   return new Promise(async (resolve, reject) => {
     try {
       const userModel = new Users();
-      if(typeof user.password !== 'undefined') {
+      if (typeof user.password !== "undefined") {
         user.password = userModel.generateHash(user.password);
       }
 
       let newUser = await Users.create(user);
-      const token = await tokens.generate({_id: newUser.id});
-      
-      newUser = Object.assign(newUser.toObject(), { tokens: {local: token} });
-      await Users.update({_id: newUser._id}, {$set: { tokens: {local: token} } });
+      const token = await tokens.generate({ _id: newUser.id });
+
+      newUser = Object.assign(newUser.toObject(), { tokens: { local: token } });
+      await Users.update(
+        { _id: newUser._id },
+        { $set: { tokens: { local: token } } }
+      );
 
       resolve(newUser);
-
-    }
-    catch(err) {
+    } catch (err) {
       console.log(err);
       reject(err);
     }
   });
-
-}
+};
 
 const registerMiddleware = async (req, res) => {
   try {
     user = req.body;
     const newUser = await register(user);
     res.send(newUser);
-  }
-  catch(err) {
+  } catch (err) {
     console.log(err);
     res.send(err);
   }
-}
+};
 
 module.exports = { register, registerMiddleware };
--- auth/routes.js
+++ auth/routes.js
@@ -1,66 +1,89 @@
-var express = require('express');
-var { registerMiddleware, validateMiddleware } = require('./index');
+var express = require("express");
+var { registerMiddleware, validateMiddleware } = require("./index");
 var router = express.Router();
-var tokens = require('./tokens');
-var models = require('../models');
+var tokens = require("./tokens");
+var models = require("../models");
 
 module.exports = function(passport) {
+  /**
+   * Validate Token
+   */
+  router.post(
+    "/validate-token",
+    tokens.validateMiddleware,
+    async (req, res) => {
+      let user = await models.Users.findById(res.locals.user._id);
+      delete user.password;
+      res.send(user).status(200);
+    }
+  );
 
-  router.post('/validate-token', tokens.validateMiddleware, async (req, res) => {
-    let user = await models.Users.findById(res.locals.user._id);
-    delete user.password;
-    res.send(user).status(200);
+  /**
+   * Login
+   */
+  router.post("/login", passport.authenticate("local"), function(req, res) {
+    res.send(req.user);
   });
 
-  router.post('/login',
-    passport.authenticate('local'),
-    function(req, res) {
-      res.send(req.user);
-    });
+  /**
+   * Register
+   */
+  router.post("/register", (req, res) => registerMiddleware(req, res));
 
-  if(process.env.NODE_ENV !== 'production') {
-    router.post('/register',
-      (req, res) => registerMiddleware(req, res)
-    );
-  }
+  /**
+   * Facebook Routes
+   */
+  router.get(
+    "/facebook",
+    passport.authenticate("facebook", { scope: ["email"] })
+  );
 
-  router.get('/facebook',
-    passport.authenticate('facebook', { scope: ['email'] }));
+  router.get("/facebook/callback", passport.authenticate("facebook"), function(
+    req,
+    res
+  ) {
+    const user = req.user.toObject();
+    res.cookie("token", user.tokens.local);
+    // res.redirect('http://localhost:4000');
+    res.redirect("http://4f654ffe.ngrok.io");
+  });
 
-  router.get('/facebook/callback',
-    passport.authenticate('facebook'),
-    function(req, res) {
-      const user = req.user.toObject();
-      res.cookie('token', user.tokens.local);
-      // res.redirect('http://localhost:4000');
-      res.redirect('http://4f654ffe.ngrok.io')
-    });
+  /**
+   * Twitter Routes
+   */
+  router.get("/twitter", passport.authenticate("twitter"));
 
-  router.get('/twitter',
-    passport.authenticate('twitter'));
+  router.get("/twitter/callback", passport.authenticate("twitter"), function(
+    req,
+    res
+  ) {
+    const user = req.user.toObject();
+    res.cookie("token", user.tokens.local);
+    res.redirect("http://localhost:4000");
+  });
 
-  router.get('/twitter/callback',
-    passport.authenticate('twitter'),
-    function(req, res) {
-      const user = req.user.toObject();
-      res.cookie('token', user.tokens.local);
-      res.redirect('http://localhost:4000');
-    });
+  /**
+   * Google Routes
+   */
+  router.get(
+    "/google",
+    passport.authenticate("google", {
+      scope: [
+        "https://www.googleapis.com/auth/calendar",
+        "https://www.googleapis.com/auth/plus.login",
+        "https://www.googleapis.com/auth/plus.profile.emails.read"
+      ]
+    })
+  );
 
-    router.get('/google',
-      passport.authenticate('google', { scope: [
-        'https://www.googleapis.com/auth/calendar',
-        'https://www.googleapis.com/auth/plus.login',
-        'https://www.googleapis.com/auth/plus.profile.emails.read'
-      ]}));
-
-    router.get('/google/callback',
-      passport.authenticate('google'),
-      function(req, res) {
-        const user = req.user.toObject();
-        res.cookie('token', user.tokens.local);
-        res.redirect('http://localhost:4000');
-      });
+  router.get("/google/callback", passport.authenticate("google"), function(
+    req,
+    res
+  ) {
+    const user = req.user.toObject();
+    res.cookie("token", user.tokens.local);
+    res.redirect("http://localhost:4000");
+  });
 
   return router;
-}
+};
